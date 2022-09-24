59ff34cbe9200ce1bb69a7bbf0de00a6b7d3c34c
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-09-24 19:52:50 +0300

    add lesson 2 to django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+    template_name = "index.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "login.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "doc_site.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "contacts.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "courses_list.html"
