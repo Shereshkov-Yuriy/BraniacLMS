2267917428004da580f05a873ab93c9ea75caec6
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-10-05 17:53:43 +0300

    lesson 7 ready

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -8,7 +9,7 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
-    path("courses_list/", views.CoursesPageView.as_view(), name="courses"),
+    path("courses_list/", cache_page(60 * 5)(views.CoursesPageView.as_view()), name="courses"),  # 5 minutes
     path("courses_list/<int:pk>/", views.CoursesDetailView.as_view(), name="courses_detail"),
     path("course_feedback/", views.CourseFeedbackFormProcessView.as_view(), name="course_feedback"),
     path("doc_site/", views.DocSitePageView.as_view(), name="docs"),
@@ -29,4 +30,6 @@ urlpatterns = [
         views.NewsDeleteView.as_view(),
         name="news_delete",
     ),
+    path("log_view/", views.LogView.as_view(), name="log_view"),
+    path("log_download/", views.LogDownloadView.as_view(), name="log_download"),
 ]
